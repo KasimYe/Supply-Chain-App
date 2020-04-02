c9a4c76ce4bd06a33fd9934dcf2f15904d9b4121
KasimYe <lip8658@live.com>
2020-04-02 23:09:45 +0800

    package

--- vue-mobile-app/src/store/index.js
+++ vue-mobile-app/src/store/index.js
@@ -20,46 +20,46 @@ export default new Vuex.Store({
     menus: [
       {
         id: 1,
-        src: require("../assets/image/icon/通讯录.png"),
+        src: require("@/assets/image/icon/tongxunlu.png"),
         name: "调度"
       },
-      { id: 2, src: require("../assets/image/icon/微信.png"), name: "装车" },
-      { id: 3, src: require("../assets/image/icon/时钟.png"), name: "追踪" },
-      { id: 4, src: require("../assets/image/icon/日历.png"), name: "加油" },
+      { id: 2, src: require("@/assets/image/icon/weixin.png"), name: "装车" },
+      { id: 3, src: require("@/assets/image/icon/shizhong.png"), name: "追踪" },
+      { id: 4, src: require("@/assets/image/icon/rili.png"), name: "加油" },
       {
         id: 5,
-        src: require("../assets/image/icon/浏览器.png"),
+        src: require("@/assets/image/icon/liulanqi.png"),
         name: "配送"
       },
       {
         id: 6,
-        src: require("../assets/image/icon/电话 (1).png"),
+        src: require("@/assets/image/icon/dianhua.png"),
         name: "收货"
       },
-      { id: 7, src: require("../assets/image/icon/相册.png"), name: "相册" },
-      { id: 8, src: require("../assets/image/icon/相机.png"), name: "相机" },
-      { id: 9, src: require("../assets/image/icon/短信.png"), name: "短信" },
+      { id: 7, src: require("@/assets/image/icon/xiangce.png"), name: "相册" },
+      { id: 8, src: require("@/assets/image/icon/xiangji.png"), name: "相机" },
+      { id: 9, src: require("@/assets/image/icon/duanxin.png"), name: "短信" },
       {
         id: 10,
-        src: require("../assets/image/icon/录制.png"),
+        src: require("@/assets/image/icon/luzhi.png"),
         name: "录音机"
       },
       {
         id: 11,
-        src: require("../assets/image/icon/youtube.png"),
+        src: require("@/assets/image/icon/youtube.png"),
         name: "Youtube"
       },
       {
         id: 12,
-        src: require("../assets/image/icon/facebook.png"),
+        src: require("@/assets/image/icon/facebook.png"),
         name: "Facebook"
       },
       {
         id: 13,
-        src: require("../assets/image/icon/BILIBILI_LOGO (1).png"),
+        src: require("@/assets/image/icon/BILIBILI_LOGO.png"),
         name: "哔哩哔哩动画"
       },
-      { id: 14, src: require("../assets/image/icon/QQ (1).png"), name: "QQ" }
+      { id: 14, src: require("@/assets/image/icon/QQ.png"), name: "QQ" }
     ]
   },
   mutations,
